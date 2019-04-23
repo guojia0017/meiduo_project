ba5fa3311916a7c70da741b89e3ccc70e61de802
xwp <xwp_fullstack@163.com>
2019-04-23 11:50:27 +0800

    sdf

--- meiduo_mall/meiduo_mall/apps/users/urls.py
+++ meiduo_mall/meiduo_mall/apps/users/urls.py
@@ -7,4 +7,5 @@ from users.views import RegisterView
 urlpatterns = [
     # 注册
     url(r'^register/$', views.RegisterView.as_view(), name='register'),
+    url(r'^/usernames/(?P<username>[a-zA-Z0-9_-]{5,20})/count/$', views.UsernameCountView.as_view(),name='username')
 ]
--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,9 +1,12 @@
+import re
+from django import http
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import AbstractUser, User
 from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
+from pymysql import DatabaseError
 
 
 class RegisterView(View):
@@ -17,13 +20,47 @@ class RegisterView(View):
         """
         return render(request, 'register.html')
     def post(self,request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        mobile = request.POST.get('mobile')
+        allow = request.POST.get('allow')
 
+        #判断参数是否齐全
+        if not all([username, password, password2, mobile, allow]):
+            return http.HttpResponseForbidden('缺少参数')
+        #判断用户名是否是５－２０个字符
+        if not re.match(r'^[a-zA-Z0-9_-]{5,20}$', username):
+            return http.HttpResponseForbidden('请输入5－２０个字符的用户名')
+        #判断密码是否为８－２０个字符
+        if not re.match(r'^[a-zA-Z0-9]{8,20}$', password):
+            return http.HttpResponseForbidden("请输入８－２０位的密码")
+        #判断二次密码
+        if password2 != password:
+            return http.HttpResponseForbidden("两次密码不一致")
+        #判断手机号
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
+            return http.HttpResponseForbidden('请输入正确的手机号码')
+        # 判断是否勾选用户协议
+        if allow != 'on':
+            return http.HttpResponseForbidden('请勾选用户协议')
+        try:
+            User.objects.create_user(username=username, password=password, mobile=mobile)
+        except DatabaseError:
+            return render(request, 'register.html', {'register_errmsg': '注册失败'})
 
-        pass
+        # 响应注册结果
+        return http.HttpResponse('注册成功，重定向到首页')
 
+class UsernameCountView(View):
+    """判断用户名是否重复注册"""
 
-# class User(AbstractUser):
-#     user = User.objects.create_user(username, email, password, **extra_fields)
-#     user = authenticate(username=username, password=password, **kwargs)
-#     class Meta(AbstractUser.Meta):
-#         swappable = 'AUTH_USER_MODEL'
+    def get(self, request, username):
+        """
+        :param request: 请求对象
+        :param username: 用户名
+        :return: JSON
+        """
+        count = User.objects.filter(username=username).count()
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'count': count})
+
